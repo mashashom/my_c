ebdaea9874c92c6bf495d9bf6c10d49527b8717b
Shomysova Maria <mashashomysova01@gmail.com>
2020-03-27 22:24:12 +0300

    init commit

---
+++ 5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define M 5
+#define N 5
+
+int main()
+{
+    int A[M][N] = {{1, 6, 5, 9, 12}, {11, 12, 92, 32,18}, {21,23, 27, 19, 61},{35, 8, 75, 4, 5}, {7, 11, 60, 32,1}};
+    int i, j, k = 0;
+
+    for (i = 0; i < M; i++){
+        for (j = 0; j < N; j++){
+            printf("%3d", A[i][j]);
+        }
+    printf("\n");
+    }
+for (j = 0; j < N; j++){
+        for (i = 0; i < M; i++){
+<<<<<<< HEAD
+            if (A[i][j] % 2 != 0){
+                k++;
+            }
+        if (k == 5){
+=======
+            if (A[i][j] % 2 == 1){
+                k++;
+            }
+        if (k == M){
+>>>>>>> 4521621da7053a9b6cc9a096c3260f12561a863f
+                printf("\n");
+                for(i = 0; i < M; i++)
+                    printf("%d\n", A[i][j]);
+                return 0;
+            }
+        }
+        k = 0;
+    }
+    return 0;
+}
